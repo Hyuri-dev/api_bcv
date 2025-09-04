1b645ad83a0666caf890a42256bc38df32eae341
Hyuri.dev <jeffersoonpaez@gmail.com>
2025-09-04 14:00:12 -0400

    modificado el main para la configuracion de render

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ app.include_router(router=tasas.router)
 @app.get("/inicio")
 async def root():
   return {"Bienvenidos al api para consulta del BCV, te recomiendo ingresar a la documentacion para visualizar mejor como funcionan los endpoints :)"}
+
+if __name__ == "__name__":
+  import uvicorn
+  uvicorn.run(app, host="0.0.0.0", port=8000)
