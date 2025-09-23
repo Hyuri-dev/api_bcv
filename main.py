41f66ab888d2010bcb7ace9d594b562fd510b3cf
Hyuri.dev <jeffersoonpaez@gmail.com>
2025-09-23 14:00:09 -0400

    corregido el dominio

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Configuramos el middleware para que el api sea accesible a los demas
 origins = [
     "http://127.0.0.1:5500",
-    "https://https://fast-bcv-ten.vercel.app/",
+    "https://fast-bcv-ten.vercel.app/",
 ]
 
 app.add_middleware(
